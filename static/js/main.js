17eb080af60f6d9a74a39a49ec3a6d8161b33f21
MALOVEROYATNO <mam229922@mail.ru>
2022-11-07 18:06:16 +0300

    qwe
    таблица и мини доработки(удаляй)

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-	const days = ["Mon","	Tues","Wed","Thurs","Fri","Sat","Sun"];
+	const days = ["Mon","Tues","Wed","Thurs","Fri","Sat","Sun"];
 
 	setInterval(() => {
 
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 
 		let month = months[nm]
-		let day = days[nd]
+		let day = days[nd - 1]
 
 		time = day + " " + h + ":" + m
 
